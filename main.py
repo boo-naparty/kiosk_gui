232b75ea35ebb58aada25f0ff56e5ca2bf0d4cc6
yjoon2k <syjoon2k@gmail.com>
2022-11-12 14:51:03 +0900

    image add

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 window = Tk()
 
 window.title("main.py")
-window.geometry("400x640+100+100")
-window.resizable(False, False)
+window.geometry("2000x1600+100+100")
+
+mp_1_img = ImageTk.PhotoImage(Image.open("./img/mainpage_1.png"))
+label = Label(window, image=mp_1_img, width=1500, height=500, fg="white")
+label.pack()
+
 window.mainloop()
