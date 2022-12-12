98d60802fc945701d5270f1ded46774d7a4bd466
yjoon2k <syjoon2k@gmail.com>
2022-12-12 21:51:58 +0900

    joo suck choo ga

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+"""
+대강 구성
+frame별로 페이지 구성이 되어있다.
+frame 안에 label 들어가고, label 안에 버튼과 구성요소들이 들어가있다.
+frame 전환 함수들을 저렇게 무식하게 때려넣은 이유는... 내가 설계를 안하고 무식하게 하나하나 하다보니깐 저래 되었다.
+"""
 def openFrame(frame):
     frame.tkraise()
 
@@ -28,12 +34,12 @@ def sidemenu_open():
 def drinkmenu_open():
    openFrame(drinkmenu_frame)   
 
-    
+#기본 Tk 구성요소
 window = Tk()
-
 window.title("main.py")
 window.geometry("500x1200+100+100")
 
+#여러가지 frame들 선언장소
 home_frame = Frame(window)
 specialmenu_frame = Frame(window)
 premiummenu_frame = Frame(window)
@@ -44,7 +50,7 @@ alldaykingmenu_frame = Frame(window)
 sidemenu_frame = Frame(window)
 drinkmenu_frame = Frame(window)
 
-
+#frame 기본 그리드설정
 home_frame.grid(row=0, column=0, sticky="nsew")
 specialmenu_frame.grid(row=0, column=0, sticky="nsew")
 premiummenu_frame.grid(row=0, column=0, sticky="nsew")
@@ -55,6 +61,8 @@ alldaykingmenu_frame.grid(row=0, column=0, sticky="nsew")
 sidemenu_frame.grid(row=0, column=0, sticky="nsew")
 drinkmenu_frame.grid(row=0, column=0, sticky="nsew")
 
+#메인화면 구성요소 선언 장소 / 메인화면이 그냥 홈 화면이다. 이미지 따오고, 버튼 추가하고 버튼 frame에 넣어놨는데 라벨로 묶어버릴지 아니면 이미지 통합할지 고민중이다
+#추후에 심영준이 수정 예정
 mp_1_img = ImageTk.PhotoImage(Image.open("./img/mainpage/mainpage_1.png"))
 mp_2_img = ImageTk.PhotoImage(Image.open("./img/mainpage/mainpage_2.png"))
 mp_3_img = ImageTk.PhotoImage(Image.open("./img/mainpage/mainpage_3.png"))
@@ -71,6 +79,39 @@ mainpage_btn3.pack()
 mainpage_btn4.pack()
 mainpage_btn5.pack()
 
+#헤더 관련 어쩌구... 일단 frame을 다 따로 나눈 이상 메뉴에서의 frame별 이동은 버튼클릭액션이 필수적이다. 바보같이 하나하나 노가다하는중
+header_special_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/special_menu_select.png"))
+header_special_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/special_menu_non.png"))
+header_new_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/new_menu_select.png"))
+header_new_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/new_menu_non.png"))
+header_premium_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/premium_menu_select.png"))
+header_premium_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/premium_menu_non.png"))
+header_whoper_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/whoper_menu_select.png"))
+header_whoper_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/whoper_menu_non.png"))
+header_chicken_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/chicken_menu_select.png"))
+header_chicken_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/chicken_menu_non.png"))
+header_alldayking_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/alldayking_menu_select.png"))
+header_alldayking_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/alldayking_menu_non.png"))
+header_side_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/side_menu_select.png"))
+header_side_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/side_menu_non.png"))
+header_drink_select_img = ImageTk.PhotoImage(Image.open("./img/menu_header/drink_menu_select.png"))
+header_drink_non_img = ImageTk.PhotoImage(Image.open("./img/menu_header/drink_menu_non.png"))
+
+menu_header_special_label = Label(specialmenu_frame, width=500, height=150, bg="white")
+menu_header_new_label = Label(newmenu_frame, width=500, height=150, bg="white")
+menu_header_premium_label = Label(premiummenu_frame, width=500, height=150, bg="white")
+menu_header_whoper_label = Label(whopermenu_frame, width=500, height=150, bg="white")
+menu_header_chicken_label = Label(chickenmenu_frame, width=500, height=150, bg="white")
+menu_header_alldayking_label = Label(alldaykingmenu_frame, width=500, height=150, bg="white")
+menu_header_side_label = Label(sidemenu_frame, width=500, height=150, bg="white")
+menu_header_drink_label = Label(drinkmenu_frame, width=500, height=150, bg="white")
+
+#여긴 아직 헤더 미완성임 / 사이즈 조절중이니깐 천천히 바꾸겠음
+header_special_select_btn = Button(menuheader_special_label, image=header_special_select_img, width=52, height=20, bg="white")
+header_special_select_btn.pack(side="top")
+menu_header_special_label.pack()
+
+#스페셜 메뉴 화면 구성요소들 / 나중에 장바구니 담는 액션이 필요하다. 
 menu_special_1_img = ImageTk.PhotoImage(Image.open("./img/menu/special_1.png"))
 menu_special_2_img = ImageTk.PhotoImage(Image.open("./img/menu/special_2.png"))
 menu_special_3_img = ImageTk.PhotoImage(Image.open("./img/menu/special_3.png"))
@@ -89,6 +130,6 @@ specialmenu_1_label.pack()
 specialmenu_2_label.pack()
 
 
-
+#기본 실행
 openFrame(home_frame)
 window.mainloop()
